339822035b89e3aaacba073813682fcb9c1b7c75
miwume <95006778+miwume@users.noreply.github.com>
2021-12-02 11:51:45 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = 'ようこそ、 ' + myName + 'さん';
   }
 }
 
@@ -30,7 +30,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'ようこそ、 ' + storedName + 'さん';
 }
 
 myButton.onclick = function() {
