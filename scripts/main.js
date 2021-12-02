d826b8f2b4c1169601bb6b1455aa14a194aa3971
miwume <95006778+miwume@users.noreply.github.com>
2021-12-02 12:22:37 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/phone.png');
-  } else {
     myImage.setAttribute ('src','images/firefox-icon.png');
+  } else {
+    myImage.setAttribute ('src','images/phone.png');
   }
 }
 
